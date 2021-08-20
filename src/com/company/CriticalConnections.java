25f601a3818ed7f1dea3d424fde3fc3d0b7118ce
Samee <sami7757@gmail.com>
2021-08-20 15:31:41 +0530

    added tarjan algo for critical connections

--- src/com/company/CriticalConnections.java
+++ src/com/company/CriticalConnections.java
@@ -1,2 +1,55 @@
-package com.company;public class CriticalConnections {
+package com.company;
+
+import java.util.*;
+
+public class CriticalConnections {
+
+    private List<Integer>[] graph;
+    private int[] visitedTime;
+    private int[] lowTime;
+    private int time;
+    List<List<Integer>> result;
+
+    public List<List<Integer>> findCriticalEdge(int n, int[][] g) {
+        graph = new ArrayList[n];
+        visitedTime = new int[n];
+        lowTime = new int[n];
+        result = new ArrayList<>();
+        time = 0;
+        buildGraph(g);
+        boolean[] visited = new boolean[n];
+        dfs(visited, 0, -1);
+        return result;
+    }
+
+    private void dfs(boolean[] visited, int currNode, int parentNode) {
+        visited[currNode] = true;
+        visitedTime[currNode] = lowTime[currNode] = time++;
+
+        for(int neighbor : graph[currNode]) {
+            if(neighbor == parentNode)
+                continue;
+
+            if(visited[neighbor]) {
+                lowTime[currNode] = Math.min(lowTime[currNode], visitedTime[neighbor]);
+            }
+            else {
+                dfs(visited, neighbor, currNode);
+                lowTime[currNode] = Math.min(lowTime[currNode], lowTime[neighbor]);
+                if(visitedTime[currNode] < lowTime[neighbor])
+                    result.add(Arrays.asList(currNode, neighbor));
+            }
+        }
+    }
+
+    private void buildGraph(int[][] g) {
+        for(int i = 0; i < graph.length; i++)
+            graph[i] = new ArrayList<>();
+
+        for(int[] edge : g) {
+            graph[edge[0]].add(edge[1]);
+            graph[edge[1]].add(edge[0]);
+        }
+    }
+
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,14 @@
 package com.company;
 
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    int n = 4;
+	    int[][] connections = { {0,1}, {0,2}, {1,2}, {1,3} };
+	    CriticalConnections cc = new CriticalConnections();
+	    List<List<Integer>> criticalEdges = cc.findCriticalEdge(n, connections);
+	    System.out.println(criticalEdges.toString());
     }
 }
